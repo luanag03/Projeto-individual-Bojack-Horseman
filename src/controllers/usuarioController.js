0ea6bb639eb920231d032fa06fd8d60d9d128b23
luanag03 <luana.araujo@sptech.school>
2025-12-01 14:20:02 -0300

    ajustando o id do usuario

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -21,7 +21,7 @@ function autenticar(req, res) {
                         console.log(resultadoAutenticar);
 
                                     res.json({
-                                        idUsuario: resultadoAutenticar[0].id,
+                                        idUsuario: resultadoAutenticar[0].idUsuario,
                                         email: resultadoAutenticar[0].email,
                                         nome: resultadoAutenticar[0].nome,
                                         senha: resultadoAutenticar[0].senha,
