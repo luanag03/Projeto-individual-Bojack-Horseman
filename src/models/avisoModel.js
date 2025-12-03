12466f7292910fcc83469abf2731715120c01b93
luanag03 <luana.araujo@sptech.school>
2025-12-02 22:35:59 -0300

    estilizando o mural e ajustando o editar do mural para ficar funcional

--- src/models/avisoModel.js
+++ src/models/avisoModel.js
@@ -72,7 +72,7 @@ function publicar(titulo, descricao, idUsuario) {
 }
 
 function editar(novaDescricao, id) {
-    console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function editar(): ", novaDescricao, idAviso);
+    console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function editar(): ", novaDescricao, id);
     var instrucaoSql = `
         UPDATE mural SET descricao = '${novaDescricao}' WHERE id = ${id};
     `;
